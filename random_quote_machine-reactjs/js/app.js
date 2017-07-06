7f1d7d5648d77137a295041b15e68018f71e6cd3
desmond <xa_yhy@163.com>
2017-07-06 10:35:54 +0800

    first reactjs demo

---
+++ random_quote_machine-reactjs/js/app.js
@@ -0,0 +1,9 @@
+var RandomQuoteApp = React.createClass({
+	render: function(){
+		return (
+			<p>Hello World</p>
+			);
+	}
+	});
+
+ReactDOM.render(<RandomQuoteApp />, document.getElementById("random-quote-machine"));
