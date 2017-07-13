568d0ff2394e0ad3683e8cb55967c0cae23fa605
desmond <xa_yhy@163.com>
2017-07-13 18:43:54 +0800

    search function added

--- wikipedia_viewer/app.js
+++ wikipedia_viewer/app.js
@@ -1,20 +1,30 @@
+// 拼接thml字符串
 var temp = "";
 
 function search() {
     
       $.ajax({
           url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $("input").val(),
+          // 跨域请求格式
           dataType: 'jsonp',
           type: 'POST',
           success: function(data){
+            // 清理上次数据
             $(".search-content").empty();
               var resultArr = data.query.search;
               for(var i in resultArr){
-                temp = "<div class='well title'><h1>"+resultArr[i].title+"</h1><p class='snippet'>"+resultArr[i].snippet+"</p></div>";
+                // temp = "<div class='well title'><h1>"+resultArr[i].title+"</h1><p class='snippet'>"+resultArr[i].snippet+"</p></div>";
+                // temp = "<div class='well title'><a href='https://en.wikipedia.org/wiki/'"+
+                // resultArr[i].title+"target='_blank'>"+"<h1>"+resultArr[i].title+"</h1>"+
+                // "</a><p class='snippet'>"+resultArr[i].snippet+"</p></div>";
+                temp = "<div class='well'><a href='https://en.wikipedia.org/wiki/"+resultArr[i].title+
+                "' target='_blank'><h3>"+resultArr[i].title+"</h3></a><p class='snippet'>"+resultArr[i].snippet+"</p></div>";
+                console.log(temp);
                 $(".search-content").append(temp);
               }
           }
       });
+      // 解绑上次事件后重新绑定
       $("input").off("keyup");
       $("input").keyup(function(){
         search();
