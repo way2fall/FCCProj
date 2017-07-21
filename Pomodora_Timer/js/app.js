51e642e4690c077d055b9d61d7c199d8746bce96
desmond <xa_yhy@163.com>
2017-07-21 19:14:11 +0800

    UI minor adjustment

--- Pomodora_Timer/js/app.js
+++ Pomodora_Timer/js/app.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-	$('#waterbubble').waterbubble();
+$(document).ready(function() {
+    $('#waterbubble').waterbubble({
+            waterColor: '#99CC33',
+            lineWidth: 2,
+            data: 0.1,
+            radius: 120,
+        }
+
+    );
 });
